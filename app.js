6c8f84bcb13759fd768fb4c47247a9223c3c566d
FacuKomorovski <facukomorovski95@gmail.com>
2024-02-08 22:31:44 -0300

    08-02

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log(1)
+const express = require("express");
+const app = express();
+
+const path = require ("path");
+
+// ruta absoluta carpeta public
+const publicPath = path.resolve(__dirname , "./public");
+
+app.use (express.static (publicPath));
+
+/*inicializo servidor 3033 
+ruta localhost:3033*/
+app.listen (3033, () => 
+    console.log("Servidor corriendo en Puerto 3033")
+);
+
+app.get("/", (req,res)=>
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+);
