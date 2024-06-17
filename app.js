2d3d16bf15eb0586358fd9ba60186cac3a737e85
FacuKomorovski <facukomorovski95@gmail.com>
2024-06-17 19:56:55 -0300

    mercadoLiebre terminado

--- app.js
+++ app.js
@@ -2,18 +2,28 @@ const express = require("express");
 const app = express();
 
 const path = require ("path");
-
-// ruta absoluta carpeta public
 const publicPath = path.resolve(__dirname , "./public");
 
 app.use (express.static (publicPath));
 
+/* inicializo PORT en render o servidor 3033 ruta localhost:3033 */
 const port = process.env.PORT || 3033;
-
 app.listen (port, () => 
-    console.log(`Servidor corriendo en Puerto ${port}}`)
+    console.log(`Servidor corriendo en Puerto ${port}`)
 );
 
-app.get("/", (req,res)=>
+app.get("/", (req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
-);
+});
+
+app.get("/register", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+app.post("/registro", (req,res)=>{
+    res.send("Registrado de manera exitosa.")
+});
+
+app.get("/login", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
